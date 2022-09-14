6438ed7f21fcc16636b71694857ec386d98e02da
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-09-14 18:16:41 -0400

    Create linoperations.cpp

---
+++ linoperations.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+int main(){
+
+    /*
+
+    unsigned int ID = getuid ();
+    printf ("ID: %d\n", ID);
+
+    */
+
+    return 0;
+}
