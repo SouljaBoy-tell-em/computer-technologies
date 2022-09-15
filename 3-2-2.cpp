4de7a700ae25ee65e4edbd27cacffb0272aea073
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-09-15 18:46:38 -0400

    Update 3-2-2.cpp

--- 3-2-2.cpp
+++ 3-2-2.cpp
@@ -12,60 +12,89 @@
 #include <string.h>
 
 
-#define LEN 4
+#define LEN 50
+
+
+void delim (char * current, char * rootCurrent);
+
 
 int main (int argc, char ** argv) {
 
-
-    const char short_option [] = "la";
-
-    const struct option long_option [] = { {"ls", no_argument, NULL, 'l'},
+    const char short_option [] = "alR";
+    const struct option long_option [] = {
+                                           {"all", no_argument, NULL, 'a'},
+                                           {"long", no_argument, NULL, 'l'},
+                                           {"recursive", no_argument, NULL, 'R'},
                                            {NULL, no_argument, NULL, 0}
-
                                          };
 
-    int long_index = 0, opt = 0, flag_a = 0, flag_l = 0;
+    int long_i = 0, opt = 0, flag_a = 0, flag_l = 0, flag_R = 0;
 
-    while ((opt = getopt_long (argc, argv, short_option, long_option, &long_index)) != -1) {
+    while ((opt = getopt_long (argc, argv, short_option, long_option, &long_i)) != -1) {
 
         switch (opt) {
+
+            case 'a': flag_a = 1;
+                      break;
 
             case 'l': flag_l = 1;
                       break;
 
-            default: break;
+            case 'R': flag_R = 1;
+                      break;
+
+            default : break;
 
         }
 
     }
 
-    char PathName [PATH_MAX];
-    char PN;
-    getwd (PathName);
+    char * current = get_current_dir_name ();
+    char rootCurrent [LEN];
+    delim (current, rootCurrent);
 
-    if (strcmp (argv[optind + 1], ".") == 0)
-        exit (EXIT_FAILURE);
+    DIR * dir;
+    dir = opendir (current);
 
-    if (flag_l == 1) {
+    if (strcmp (argv[1], "..") == 0)
+        dir = opendir (rootCurrent);
 
-        DIR * file;
-        struct dirent * get;
-        file = opendir (PathName);
+    struct dirent * getDir;
+    while ((getDir = readdir (dir)) != NULL) {
 
-        while (file != NULL) {
+        if (flag_a != 1)
+            if (strcmp (getDir->d_name, ".") == 0 || strcmp (getDir->d_name, "..") == 0)
+                continue;
 
-            get = readdir (file);
-            if (!get)
-                break;
-
-            printf ("%s\n", get->d_name);
-        
-        }
-
-        closedir (file);
-
+        printf ("%s\n", getDir->d_name);
     }
 
     return 0;
+}
+
+void delim (char * current, char * rootCurrent) {
+
+    int i = 0, Delim = 0, flagDelim = 0;
+
+    for (i = 0; i < strlen (current); i++)
+        if ( * (current + i) == '/')
+            Delim++;
+
+    for (i = 0; i < strlen (current); i++) {
+
+        if ( * (current + i) == '/')
+            flagDelim++;
+
+        if (flagDelim == Delim) {
+
+            * (rootCurrent + i) = '\0';
+            break;
+
+        }
+
+        * (rootCurrent + i) = * (current + i);
+
+    }
+
 
 }
