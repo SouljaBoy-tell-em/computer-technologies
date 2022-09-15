b003cea4ada8ad3db65c088e54a7892a256b46f9
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-09-15 04:06:50 -0400

    Update linoperations.cpp

--- linoperations.cpp
+++ linoperations.cpp
@@ -1,16 +1,54 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+
 
 
 int main(){
 
     /*
 
-    unsigned int ID = getuid ();
-    printf ("ID: %d\n", ID);
+    printf ("user ID: %d\n", getuid());
 
     */
 
+    /*
+
+    struct passwd * pt = getpwuid (getuid ());
+
+    printf ("user password: %s\n", pt->pw_passwd);
+    printf ("user name: %s\n", pt->pw_name);
+    printf ("user ID: %d\n", pt->pw_uid);
+    printf ("group ID: %d\n", pt->pw_gid);
+    printf ("real name: %s\n", pt->pw_gecos);
+    printf ("home directory: %s\n", pt->pw_dir);
+    printf ("shell prorgam: %s\n", pt->pw_shell);
+
+    */
+
+    /*
+
+    printf ("group ID: %d\n", getgid ());
+
+    */
+
+
+    struct group * pts = getgrgid (getgid ());
+
+    printf ("group name: %s\n", pts->gr_name);
+    printf ("password of group: %s\n", pts->gr_passwd);
+    printf ("identification of group: %d\n", pts->gr_gid);
+    printf ("group %s", *(pts->gr_mem));
+
+    while ( * (pts->gr_mem) != NULL) {
+
+        printf ("group %s", *(pts->gr_mem));
+        (* (pts->gr_mem))++;
+
+    }
+
     return 0;
+    
 }
