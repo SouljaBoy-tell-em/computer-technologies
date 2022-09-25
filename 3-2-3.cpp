13a70b0a5ef15ec1068d36f3159e3e4d73744ad7
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-09-26 02:51:48 +0300

    Update 3-2-3.cpp

--- 3-2-3.cpp
+++ 3-2-3.cpp
@@ -13,13 +13,14 @@
 
 
 #define LEN 50
+#define CP "cp"
 
 
 #define CHECK_ERROR(condition, message_error, error_code) \
             do {                                          \
                if (condition) {                           \
                    printf ("%s", message_error);          \
-                   return error_code;                     \
+                   exit (error_code);                     \
                }                                          \
             } while(false)
 
@@ -35,6 +36,9 @@ enum error_code {
 unsigned long FileSize (FILE * file, struct stat * buf);
 size_t copy (char ** argv);
 void info (void);
+void command_block (int argc, char ** argv, const char * short_option, const struct option * long_option);
+void save_specificator (char ** argv);
+char * infoSpecificatorFile (FILE * save);
 
 
 int main (int argc, char ** argv) {
@@ -45,7 +49,17 @@ int main (int argc, char ** argv) {
                                            {"verbose", no_argument, NULL, 'v'},
                                            {NULL, no_argument, NULL, 0}
                                          };
+
+    command_block (argc, argv, short_option, long_option);
+
+    return 0;
+}
+
+
+void command_block (int argc, char ** argv, const char * short_option, const struct option * long_option) {
+    
     int long_i = 0, opt = 0, flag_h = 0, flag_v = 0;
+
     while ((opt = getopt_long (argc, argv, short_option, long_option, &long_i)) != -1) {
 
         switch (opt) {
@@ -63,8 +77,42 @@ int main (int argc, char ** argv) {
     if (flag_h == 1)
         info ();
     
-    //copy (argv);
-    return 0;
+    if (flag_v == 1) {
+
+        save_specificator (argv);
+    }
+    
+    if (argc >= 3)
+        copy (argv);
+}
+
+
+char * infoSpecificatorFile (FILE * save) {
+    
+    char ch, * save_str = (char * ) malloc (sizeof (char) * LEN);
+
+    while ((ch = getc (save)) != EOF) {
+        
+        * save_str = ch;
+        putchar (* save_str);
+        save_str++;    
+    }
+    * save_str = '\0';
+    
+    //puts (save_str);
+    return save_str;    
+}
+
+
+void save_specificator (char ** argv) {
+
+    FILE * save = fopen ("save.txt", "a");
+    CHECK_ERROR (save == NULL, "Problem with opening file.", FILE_AREN_T_OPENING);
+    fprintf (save, "%s %s %s", argv[optind], ">", argv[optind + 1]);
+    char * mem = (char * ) malloc (sizeof (char) * 10);
+    fread (mem, sizeof (char), 100, save);
+    fputs (mem, stdout); 
+    fclose (save);
 }
 
 
@@ -101,4 +149,7 @@ size_t copy (char ** argv) {
 void info (void) {
     
     puts ("This program can copy information between 2 files.");
+    puts ("If you want copy information between something    ");
+    puts ("files, you should point specificator CP in small  ");
+    puts ("letters. Struct description: file1 cp file2       ");
 }
