f73dcb5e60d6c4015ae173a4321be584a8b345b8
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-09-27 13:12:41 +0300

    Update 3-2-3.cpp

--- 3-2-3.cpp
+++ 3-2-3.cpp
@@ -33,10 +33,11 @@ enum error_code {
 };
 
 
-unsigned long FileSize (FILE * file, struct stat * buf);
-size_t copy (char ** argv);
-void info (void);
 void command_block (int argc, char ** argv, const char * short_option, const struct option * long_option);
+size_t copy (char ** argv);
+unsigned long FileSize (FILE * file);
+void info (void);
+void outputCommand (void);
 void save_specificator (char ** argv);
 
 
@@ -48,7 +49,6 @@ int main (int argc, char ** argv) {
                                            {"verbose", no_argument, NULL, 'v'},
                                            {NULL, no_argument, NULL, 0}
                                          };
-
     command_block (argc, argv, short_option, long_option);
 
     return 0;
@@ -57,7 +57,7 @@ int main (int argc, char ** argv) {
 
 void command_block (int argc, char ** argv, const char * short_option, const struct option * long_option) {
     
-    int long_i = 0, opt = 0, flag_h = 0, flag_v = 0;
+    int long_i = 0, opt = 0, flag_h = 0, flag_v = 0, flag_i = 0;
 
     while ((opt = getopt_long (argc, argv, short_option, long_option, &long_i)) != -1) {
 
@@ -78,29 +78,11 @@ void command_block (int argc, char ** argv, const char * short_option, const str
     
     if (flag_v == 1) {
 
-        save_specificator (argv);
+        outputCommand ();
     }
     
     if (argc >= 3)
         copy (argv);
-}
-
-
-void save_specificator (char ** argv) {
-
-    FILE * save = fopen ("save.txt", "rb+");
-    CHECK_ERROR (save == NULL, "Problem with opening file.", FILE_AREN_T_OPENING);
-    fprintf (save, "%s %s %s", argv[optind], ">", argv[optind + 1]);
-    fclose (save);
-}
-
-
-unsigned long FileSize (FILE * file, struct stat * buf) {
-
-    if (fstat (fileno (file), buf) == 0)
-        return buf->st_size;
-
-    return 0;
 }
 
 
@@ -112,16 +94,33 @@ size_t copy (char ** argv) {
     strcpy (rec2, argv [optind + 1]);
 
     FILE * file1 = fopen (rec1, "r");
-    CHECK_ERROR (file1 == NULL, "Problem with opening file.", FILE_AREN_T_OPENING);
+    CHECK_ERROR (file1 == NULL, "Problem with opening file1.txt", FILE_AREN_T_OPENING);
     FILE * file2 = fopen (rec2, "rb+");
-    CHECK_ERROR (file2 == NULL, "Problem with opening file.", FILE_AREN_T_OPENING);
-    unsigned long size = FileSize (file1, &buf);
+    CHECK_ERROR (file2 == NULL, "Problem with opening file file2.txt", FILE_AREN_T_OPENING);
+    FILE * save = fopen ("save.txt", "w");
+    CHECK_ERROR (save == NULL, "Problem with opening file save.txt", FILE_AREN_T_OPENING);
+    unsigned long size = FileSize (file1);
     char * mem = (char * ) malloc (size * sizeof (char));
     CHECK_ERROR (mem == NULL, "Problem with opening file.", NOT_MEMORY); 
     fread (mem, sizeof (char), size, file1);
     fwrite (mem, sizeof (char), size, file2);
+    fprintf (save, "%s > %s", rec1, rec2);
+
+    fclose (file1);
+    fclose (file2);
+    fclose (save);
 
     return NO_ERROR;
+}
+
+unsigned long FileSize (FILE * file) {
+    
+    struct stat buf = {};
+
+    if (fstat (fileno (file), &buf) == 0)
+        return buf.st_size;
+
+    return 0;
 }
 
 
@@ -131,4 +130,16 @@ void info (void) {
     puts ("If you want copy information between something    ");
     puts ("files, you should point specificator CP in small  ");
     puts ("letters. Struct description: file1 cp file2       ");
+}
+
+
+void outputCommand (void) {
+
+    FILE * save = fopen ("save.txt", "r");
+    CHECK_ERROR (save == NULL, "Problem with opening file save.txt", FILE_AREN_T_OPENING);
+    unsigned long filesize = FileSize (save);
+    char * mem = (char * ) calloc (filesize, sizeof (char));
+    fread (mem, sizeof (char), filesize, save);
+    puts (mem);
+    free (mem);
 }
